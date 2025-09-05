fc0f9bd5b099c64695c802349ac117a33cadbe11
Davi Monteiro <davimonteiro0734@gmail.com>
2025-09-05 03:03:04 +0000

    criei  o printf

--- n2_codar.C
+++ n2_codar.C
@@ -1,1 +1,28 @@
+#include  <stdio.h>
 
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("digite seu nome: \n"),
+    scanf("%s", &nome);
+
+    printf("digite sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("nome do aluno: %s - matrícula: %d", nome, matricula);
+    printf("idade: %d - altura: %f", idade, altura);
+
+    return 0;
+
+
+
+
+}
